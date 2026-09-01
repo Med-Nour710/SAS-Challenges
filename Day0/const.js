5f5c6afc970eeb1d46f45212bf99fa211e0d33f5
Mohamed Nour <pearcejosh29@gmail.com>
2026-09-01 11:08:28 +0100

    Exercice for const keyword

---
+++ Day0/const.js
@@ -0,0 +1,10 @@
+//Managing Monthly Expenes of a Group of 5 "Zwafriya"
+const num_zwaf = 5;
+const rent = 3000;
+let food = 500;
+let electricity = 50;
+let water = 40;
+let misc = 200;
+let total = food + electricity + water + misc ;
+let individual_obligation = total / 5;
+console.log( individual_obligation)
