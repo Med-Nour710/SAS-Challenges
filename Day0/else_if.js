a1d83da811437bcc047ade769f0f89a8f4b5f54c
Mohamed Nour <pearcejosh29@gmail.com>
2026-09-01 13:37:17 +0100

    Exercice for else if

---
+++ Day0/else_if.js
@@ -0,0 +1,23 @@
+//Age Clasification
+let age = 24;
+
+if (age > 0 && age < 10)
+{
+    console.log("You are Still a Child")
+}
+ else if(age > 10 && age < 18 )
+ {
+    console.log( "You are a Teenager")
+ }
+ else if(age > 18 && age < 30)
+ {
+    console.log( "You are a Young Adult")
+ }
+ else if(age > 30 && age < 50)
+ {
+    console.log("You are a Full Grown Adult")
+ }
+ else if(age > 50 && age < 70)
+ {
+    console.log("You are an Elder")
+ }
