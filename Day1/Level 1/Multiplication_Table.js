58bebae8426a704adc55e59723dfc0c57df19459
Mohamed Nour <pearcejosh29@gmail.com>
2026-09-02 15:04:04 +0100

    Level 1 exercices for Day1

---
+++ Day1/Level 1/Multiplication_Table.js
@@ -0,0 +1,5 @@
+let m_n = 4
+for(m_v=1;m_v<11;m_v++)
+    {
+        console.log(m_n + " x " + m_v + " = " + m_n*m_v)
+}
---
+++ Day1/Level 1/Sum_of_1st_hundred.js
@@ -0,0 +1,5 @@
+let total = 0;
+for(a=1;a<101;a++) {
+total +=a;
+}
+console.log(total)
